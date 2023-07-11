09d2b11d0ba723ad94ceabcf29f4e55a5918d17b
Bárbara Guerbas de Figueiredo <baguerbassita@gmail.com>
2023-07-11 15:42:28 -0300

    working with variables in templates

--- recipes/views.py
+++ recipes/views.py
@@ -15,5 +15,8 @@ def recipe(request, id):
     return render(
         request,
         'recipes/pages/recipe-view.html',
-        context={'recipe': make_recipe()}
+        context={
+            'recipe': make_recipe(),
+            'is_detail_page': True,
+            }
         )
