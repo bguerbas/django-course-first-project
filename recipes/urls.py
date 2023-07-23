6afa445be969a8e0a2f7f7780e27976787b9a846
Bárbara Guerbas de Figueiredo <baguerbassita@gmail.com>
2023-07-22 23:38:32 -0300

    remove factory.py and update tests

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='recipes-home'),
+    path('recipes/search/', views.search, name='recipes-search'),
     path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
     path('recipes/category/<int:category_id>/', views.category,
-         name='recipes-category')
+         name='recipes-category'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.db.models import Q  
 
 from .models import Recipe
 
@@ -39,3 +41,25 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+    if not search_term:
+        raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        # busca por título que contenha o termo (se colocar title = search_term, busca exata)  # noqa: E501
+        # o 'i' no final do icontains significa case insensitive (ignora maiúsculas e minúsculas) # noqa: E501
+        Q(
+            Q(title__icontains=search_term) |  # o "Q" serve para indicar a busca por OU # noqa: E501
+            Q(description__icontains=search_term)
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
+        })
