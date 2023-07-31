d145b7769c0f8027e1420db7d2b5b8275b7e4fc9
Bárbara Guerbas de Figueiredo <baguerbassita@gmail.com>
2023-07-31 15:06:38 -0300

    complete messages forms

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
+
 from .forms import RegisterForm
 
 
@@ -13,10 +15,16 @@ def register_view(request):
 
 def register_create(request):
     if not request.POST:
-        raise Http404('Page not found')
+        raise Http404()
 
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del (request.session['register_form_data'])
 
     return redirect('authors:register')
